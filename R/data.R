641938a5bb35116b0caabd6b09a99688abae14b8
ronaldoalves-ms <71024351+ronaldoalves-ms@users.noreply.github.com>
2021-05-02 16:17:44 -0300

    add dados

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @title Codificação de Causas de Óbitos Evitáveis na Infância
+#'
+#' @description Um data frame com códigos de subcategorias da CID-10.
+#'
+#' @format Um data frame com 12451 linhas e 1 variável:
+#' \describe{
+#'   \item{var_nome}{Códigos de subcategorias da CID-10}
+#' }
+#' @source <https://www.github.com/ronaldoalves-ms/stopdeath>
+"dados"
